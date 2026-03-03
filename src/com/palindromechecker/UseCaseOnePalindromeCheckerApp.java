ba6429f4976246bc4aea5e982243d870a7496e65
RITHVIK-KASHYAP <rithvik052003@gmail.com>
2026-03-03 17:00:42 +0530

    Coded UC1 to print Welcome Message

---
+++ src/com/palindromechecker/UseCaseOnePalindromeCheckerApp.java
@@ -0,0 +1,39 @@
+package com.palindromechecker;
+
+/**
+ * Main Class - Use Case 1 PalindromeCheckerApp
+ * 
+ * Use Case 1: Application Entry & Welcome Message
+ * 
+ * Description:
+ * This class represents the entry point of the Palindrome Checker Management System.
+ * 
+ * At this stage, the application:
+ * 	- Starts execution from the main() method
+ * 	- Displays a welcome message
+ * 	- Shows application version
+ * 
+ * No palindrome logic is implemented yet.
+ * 
+ * The goal is to establish a clear startup flow.
+ * 
+ * @author Rithvik
+ * @version 1.0
+ */
+
+public class UseCaseOnePalindromeCheckerApp {
+	/**
+	 * Application Entry Point
+	 * 
+	 * This is the first method executed by the JVM when the program starts.
+	 * 
+	 * @param args Command-line arguments
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Palindrome Checker Management System");
+		System.out.println("Version : 1.0");
+		System.out.println("System initialized successfully");
+	}
+	
+}
