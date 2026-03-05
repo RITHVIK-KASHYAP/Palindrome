c5a4d696749187062fbb4420dbed298d54a644a9
RITHVIK-KASHYAP <rithvik052003@gmail.com>
2026-03-05 11:49:26 +0530

    Coded UC7 to check for a palindrome using deque

---
+++ src/com/palindromechecker/UseCaseSevenPalindromeCheckerApp.java
@@ -0,0 +1,57 @@
+package com.palindromechecker;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Main Class - Use Case 7
+ * 
+ * UseCase7: Deque-Based Optimized Palindrome Checker
+ * 
+ * Description:
+ * This class validates a palindrome using a Deque (Double Ended Queue).
+ * 
+ * Characters are inserted into the deque and then compared by
+ * removing elements from both ends:
+ * 	-removeFirst()
+ * 	-removeLast()
+ * 
+ * 
+ * This avoids reversing the string and provides an efficient front-to-back
+ * comparison approach.
+ * 
+ * This use case demonstrates optimal optimal bidirectional traversal using
+ * deque.
+ * 
+ * @author Developer
+ * @version 7.0
+ */
+
+public class UseCaseSevenPalindromeCheckerApp 
+{
+	/**
+	 * Application entry point for UC5.
+	 * 
+	 * @param args Command-line arguments
+	 */
+	public static void main(String[] args) 
+	{
+		String input = "refer";
+		Deque<Character> deque = new ArrayDeque<Character>();
+		for (char c : input.toCharArray()) 
+		{
+			deque.add(c);
+		}
+		boolean isPalindrome = true;		
+		while (deque.size() > 1) 
+		{
+			if (!(deque.removeFirst().equals(deque.removeLast()))) 
+			{
+				isPalindrome = false;
+				break;
+			}
+		}		
+		System.out.println("Input: " + input);
+		System.out.println("Is Palindrome? " + (isPalindrome ? "true" : "false"));
+	}
+}
